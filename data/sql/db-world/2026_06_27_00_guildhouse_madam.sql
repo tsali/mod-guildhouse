ffabe7821187cd1b2d0898086979dced89746a5c
tsali <raptor@isle.dev>
2026-06-27 16:52:26 -0500

    Add Madam guild-house spawn point SQL (was missed - git add)

---
+++ data/sql/db-world/2026_06_27_00_guildhouse_madam.sql
@@ -0,0 +1,3 @@
+DELETE FROM `guild_house_spawns` WHERE `entry`=700050;
+INSERT INTO `guild_house_spawns` (`id`,`entry`,`posX`,`posY`,`posZ`,`orientation`,`comment`) VALUES
+(100, 700050, 16240, 16295, 13.176, 3.0, 'Madam');
